4440453d1e36073b1e573ae739a560af3d5974dd
YUN\yun <1097598984@qq.com>
2018-05-21 18:20:33 +0800

    init

---
+++ springcloud-eureka-client/src/main/java/com/yunhui/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.yunhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Yun
+ * @Description:
+ * @Date: Created in 2018-05-21 18:09
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
